6d9dc3cff40077260015a362d249b98770b3485a
musfira <musfiraansari05@gmail.com>
2024-08-09 00:48:22 +0500

    fix: folder

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Footer from "./Footer/Footer";
 import Services from "./AppServices/Services";
-import Header from "./Header/Header";
 import Topbar from "./Navigation/Topbar";
 import Menubar from "./Navigation/Menubar";
 import Submenu from "./Navigation/Submenu";
 import Offerbar from "./Navigation/Offerbar";
+import  Carousel  from "./Header/Carousel";
 
 const Home = () => {
   return (
     <>
       <Topbar />
-      <Menubar/>
-      <Submenu/>
-      <Offerbar/>
-      <Header />
+      <Menubar />
+      <Submenu />
+      <Offerbar />
+      <Carousel />
       <Services />
       <Footer />
     </>
